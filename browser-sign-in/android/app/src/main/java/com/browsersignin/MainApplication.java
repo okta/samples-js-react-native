5cd56390f8bdf4b02965f91f46900f4ad9362949
Ildar Abdullin <Ildar.Abdullin@okta.com>
2019-09-10 16:51:31 -0700

    OKTA-246008 - ReactNative OIDC SDK: Update RN sample app with sdk 1.1.0

--- browser-sign-in/android/app/src/main/java/com/browsersignin/MainApplication.java
+++ browser-sign-in/android/app/src/main/java/com/browsersignin/MainApplication.java
@@ -28,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      packages.add(new OktaSdkBridgePackage());
+      // packages.add(new OktaSdkBridgePackage());
       return packages;
     }
 
