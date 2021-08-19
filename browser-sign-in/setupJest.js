5b04ea7d3309baa98c96acb6275779cf92bfbc06
Oleg Gnidets <oleg.gnidets@okta.com>
2021-08-19 18:50:47 +0300

    tests: Adds extra mock

--- browser-sign-in/setupJest.js
+++ browser-sign-in/setupJest.js
@@ -23,34 +23,35 @@ global.fetch = jest.fn();
 if (typeof window !== 'object') {
   global.window = global;
   global.window.navigator = {};
+  global.window.location = {};
 }
 
-import * as ReactNative from "react-native";
+import * as ReactNative from 'react-native';
 
 jest.doMock('react-native', () => {
   // Extend ReactNative
   return Object.setPrototypeOf(
-      {
-        NativeModules: {
-          ...ReactNative.NativeModules,
-          OktaSdkBridge: {
-            createConfig: jest.fn(),
-            signIn: jest.fn(),
-            signOut: jest.fn(),
-            getAccessToken: jest.fn(),
-            getIdToken: jest.fn(),
-            getUser: jest.fn(),
-            isAuthenticated: jest.fn(),
-            revokeAccessToken: jest.fn(),
-            revokeIdToken: jest.fn(),
-            revokeRefreshToken: jest.fn(),
-            introspectAccessToken: jest.fn(),
-            introspectIdToken: jest.fn(),
-            introspectRefreshToken: jest.fn(),
-            refreshTokens: jest.fn(),
-          },
+    {
+      NativeModules: {
+        ...ReactNative.NativeModules,
+        OktaSdkBridge: {
+          createConfig: jest.fn(),
+          signIn: jest.fn(),
+          signOut: jest.fn(),
+          getAccessToken: jest.fn(),
+          getIdToken: jest.fn(),
+          getUser: jest.fn(),
+          isAuthenticated: jest.fn(),
+          revokeAccessToken: jest.fn(),
+          revokeIdToken: jest.fn(),
+          revokeRefreshToken: jest.fn(),
+          introspectAccessToken: jest.fn(),
+          introspectIdToken: jest.fn(),
+          introspectRefreshToken: jest.fn(),
+          refreshTokens: jest.fn(),
         },
       },
-      ReactNative,
+    },
+    ReactNative,
   );
 });
